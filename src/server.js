826a043620e29b8d41178b6d589adb5403678270
fheldtm <fheldtm@gmail.com>
2022-04-10 04:30:01 +0900

    first commit

---
+++ src/server.js
@@ -0,0 +1,28 @@
+import http from 'http'
+import WebSocket from 'ws'
+import express from 'express'
+
+const app = express()
+
+app.set("view engine", "pug")
+app.set("views", __dirname + "/views")
+
+app.use("/public", express.static(__dirname + '/public'))
+
+app.get("/", (req, res) => {
+  res.render("home")
+})
+
+const server = http.createServer(app)
+const wss = new WebSocket.Server({ server })
+
+wss.on("connection", (socket) => {
+  console.log('Connected to Browser!')
+  socket.on('close', () => { console.log('Disconnected from the Browser') })
+  socket.on('message', message => {
+    console.log(message.toString('utf-8'), ' from the Browser')
+  })
+  socket.send('Hello!')
+})
+
+server.listen(3000)
